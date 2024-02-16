22a4150efbfd690581c628fd5c5baf8f55df5456
kuang-ji <131479266+kuang-ji@users.noreply.github.com>
2024-02-16 22:35:29 +0900

    Update redroid.sh

--- redroid.sh
+++ redroid.sh
@@ -5,15 +5,16 @@ sudo apt update
 sudo apt upgrade -y
 
 # 安装 Linux 内核模块
-sudo apt install -y linux-modules-extra-$(uname -r)
+sudo apt install -y linux-modules-extra-`uname -r`
 sudo modprobe binder_linux devices="binder,hwbinder,vndbinder"
+sudo modprobe ashmem_linux
 
 # 安装 Docker
 curl -fsSL https://get.docker.com | sh
 
 # 运行 Docker 容器
 sudo docker run -itd --privileged \
-    #--memory-swappiness=0 \
+    --memory-swappiness=0 \
     --restart=always \
     --pull always \
     -v ~/data13:/data \
